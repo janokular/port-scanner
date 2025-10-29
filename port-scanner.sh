bfe01e857db95786472dd144b68f651bf96b292f
janokular <janekokularczyk@gmail.com>
2025-10-29 13:33:57 +0100

    updated port-scanner.sh

--- port-scanner.sh
+++ port-scanner.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-nc -z -v -w 1 localhost 0-65535
+# localhost TCP port scanner
+
+host=localhost
+
+if [[ $# -ne 2 ]]; then
+  echo 'Provide two arguments'
+  exit 1
+fi
+
+start_port=$1
+end_port=$2
+
+echo 'Scanning ports...'
+for (( port=$start_port; port<=$end_port; port++ )); do
+  timeout 1 bash -c "< /dev/tcp/$host/$port" 2&> /dev/null
+  if [[ $? -eq 0 ]]; then
+    echo "$host has open port $port"
+  fi
+done
